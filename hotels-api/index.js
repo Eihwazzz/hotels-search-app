64d52dd10a274193d5863b0f74b32f563ca5a8ea
eihwazzz <mike4hunter@hotmail.com>
2019-04-07 13:49:14 -0300

    Se agrega express dependency al cliente. se agrega .env para levantar puerto configurable para diferentes ambientes

--- hotels-api/index.js
+++ hotels-api/index.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const jsonData = require('./functions/getData')
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 3000;
+const port = process.env.PORT || 3001;
 var data = jsonData.getData();
 
 	
--- hotels-app/server.js
+++ hotels-app/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var path = require('path');
-var app = express();
+require('dotenv').config()
+const express = require('express');
+const path = require('path');
+const app = express();
 
 const port = process.env.PORT || 3001;
 
